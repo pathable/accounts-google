4509b3871dfa940391619a7475dde5fcbbf49bee
Luiz <luizcarlos1405@gmail.com>
2020-09-02 15:57:11 -0300

    Point to pathable:google-oauth instead

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Login service for Google accounts",
-  version: "1.3.3",
+  version: "1.0.0",
 });
 
 Package.onUse(api => {
@@ -9,7 +9,7 @@ Package.onUse(api => {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('google-oauth');
+  api.use('pathable:google-oauth@1.0.0');
   api.imply('google-oauth');
 
   // If users use accounts-ui but not google-config-ui, give them a tip.
