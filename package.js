2aa285a9376b4a9cc803ae9b0b705d64fc71b796
Igor Fontana <igor193@gmail.com>
2021-08-05 08:03:23 -0300

    TP-15511 Upgrade to Meteor 2.3 - upgrade oauth

--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
   summary: "Login service for Google accounts",
-  version: "1.0.3",
+  version: "1.1",
   name: 'pathable:accounts-google',
   git: 'https://github.com/pathable/accounts-google',
 });
 
 Package.onUse(api => {
-  api.versionsFrom('1.10.3-beta.9');
+  api.versionsFrom('2.3.4');
 
   api.use(['ecmascript']);
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('pathable:google-oauth@1.0.3');
+  api.use('pathable:google-oauth@1.1.0');
   api.imply('pathable:google-oauth');
 
   // If users use accounts-ui but not google-config-ui, give them a tip.
